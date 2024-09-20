f6fa270afb6747756cd6f40d6cf4854791ddf393
peonymyx <peonymyx@gmail.com>
2024-09-21 01:12:21 +0700

    Database by Prisma

---
+++ Backend/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+require('dotenv').config();
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  console.log(`Server running on ${process.env.PROTOCAL}${process.env.HOST}:${process.env.PORT}`);
+  await app.listen(3000);
+}
+bootstrap();
